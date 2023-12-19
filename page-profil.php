62ab2ff994590fcc3edef4be58a0b049f2dc63fb
Quentin Garnier <113943161+GarnierQuentin@users.noreply.github.com>
2023-12-19 16:38:31 +0100

    🎨 adding profile page

--- page-profil.php
+++ page-profil.php
@@ -4,20 +4,28 @@ get_header();
 
 if (is_user_logged_in()) {
     // L'utilisateur est connecté, affichez du contenu réservé aux utilisateurs connectés ici
+    ?>
 
-    echo '<h1>Bienvenue, ' . esc_html(wp_get_current_user()->display_name) . ' !</h1>';
+    <h1>MON PROFIL</h1>
+    <h3> Mes informations</h3>
 
+    <div class="informations">
+        <div class="infos_de_contact">
+            <?php
+            echo "<div class='user_name'>" . wp_get_current_user()->display_name . "</div>";
+            echo "<div class='user_mail'>" . wp_get_current_user()->user_email . "</div>";
+            ?>
+        </div>
+    </div>
+<?php
 
 } else {
     // L'utilisateur n'est pas connecté, affichez un message ou le formulaire de connexion ici
     ?>
     <body>
-
         <div>
             <p>Veuillez vous connecter ou créer un compte pour accéder au contenu réservé aux utilisateurs connectés.</p>
         </div>
-
-
         <div>
             <button>
                 <a href="connexion">Se connecter</a>
@@ -28,7 +36,6 @@ if (is_user_logged_in()) {
             </button>
         </div>
     </body>
-
     <?php
 }
 
