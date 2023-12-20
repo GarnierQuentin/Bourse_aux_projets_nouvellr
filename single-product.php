c3e0b0b24c6999420bef4b76464f66e7a4861c08
Mohsen Hosen <116373904+HosenMohsen@users.noreply.github.com>
2023-12-21 00:45:20 +0100

    UPDATE page produit desktop and responsive

---
+++ single-product.php
@@ -0,0 +1,400 @@
+<?php
+
+get_header();
+?>
+
+    <main class="main_produit">
+
+
+
+	
+	<div class="card_produit">
+        <div class="container">
+            <div class="carousel">
+                <input type="radio" name="slides" checked="checked" id="slide-1">
+                <input type="radio" name="slides" id="slide-2">
+                <input type="radio" name="slides" id="slide-3">
+                <input type="radio" name="slides" id="slide-4">
+                <input type="radio" name="slides" id="slide-5">
+                <input type="radio" name="slides" id="slide-6">
+                <ul class="carousel__slides">
+                    <li class="carousel__slide">
+                        <figure>
+                            <div>
+                                <img src="https://picsum.photos/id/1041/800/450" alt="">
+                            </div>
+                            <figcaption>
+                            <div class="card_produit_content">
+                
+                                <div class="card_produit_content_texte">
+                        
+                                    <h3> 4 soucoupes en porcelaine</h3>
+                                    <h4> 4 soucoupes en porcelaine en bon état.</h4>
+                                    <hr class="produit_hr">
+                                    <p> 0.50€</p>
+                                    <hr class="produit_hr">
+                                    <div class="card_produit_content_texte_liste">
+                                        <ul>
+                                            <li> Marque</li>
+                                            <li> État </li>
+                                            <li> Matière</li>
+                                            <li> Nombres de vues</li>
+                                            <li> Ajouté</li>
+                                        </ul>
+                                        <ul>
+                                            <li> Aucune</li>
+                                            <li> Bon état</li>
+                                            <li> Porcelaine</li>
+                                            <li> 34</li>
+                                            <li> il y a 11h</li>
+                                        </ul>
+                                    </div>
+                                    <div class="text_produit">
+                                        <p> Autres détails</p>
+                                    </div>
+                                    
+                                
+                                    <div class="produit_link">
+                                    <a  href=""> Réserver</a>
+                                    </div>
+                                
+                                
+                                </div>
+
+                
+                            </div>
+                            </figcaption>
+                        </figure>
+                    </li>
+                    <li class="carousel__slide">
+                        <figure>
+                            <div>
+                                <img src="https://picsum.photos/id/1043/800/450" alt="">
+                            </div>
+                            <figcaption>
+                            <div class="card_produit_content">
+                
+                <div class="card_produit_content_texte">
+        
+                    <h3> 4 soucoupes en porcelaine</h3>
+                    <h4> 4 soucoupes en porcelaine en bon état.</h4>
+                    <hr class="produit_hr">
+                    <p> 0.50€</p>
+                    <hr class="produit_hr">
+                    <div class="card_produit_content_texte_liste">
+                        <ul>
+                            <li> Marque</li>
+                            <li> État </li>
+                            <li> Matière</li>
+                            <li> Nombres de vues</li>
+                            <li> Ajouté</li>
+                        </ul>
+                        <ul>
+                            <li> Aucune</li>
+                            <li> Bon état</li>
+                            <li> Porcelaine</li>
+                            <li> 34</li>
+                            <li> il y a 11h</li>
+                        </ul>
+                    </div>
+                    <div class="text_produit">
+                        <p> Autres détails</p>
+                    </div>
+                    
+                
+                    <div class="produit_link">
+                    <a  href=""> Réserver</a>
+                    </div>
+                
+                
+                </div>
+
+    
+            </div>
+                            </figcaption>
+                        </figure>
+                    </li>
+                    <li class="carousel__slide">
+                        <figure>
+                            <div>
+                                <img src="https://picsum.photos/id/1044/800/450" alt="">
+                            </div>
+                            <figcaption>
+                            <div class="card_produit_content">
+                
+                <div class="card_produit_content_texte">
+        
+                    <h3> 4 soucoupes en porcelaine</h3>
+                    <h4> 4 soucoupes en porcelaine en bon état.</h4>
+                    <hr class="produit_hr">
+                    <p> 0.50€</p>
+                    <hr class="produit_hr">
+                    <div class="card_produit_content_texte_liste">
+                        <ul>
+                            <li> Marque</li>
+                            <li> État </li>
+                            <li> Matière</li>
+                            <li> Nombres de vues</li>
+                            <li> Ajouté</li>
+                        </ul>
+                        <ul>
+                            <li> Aucune</li>
+                            <li> Bon état</li>
+                            <li> Porcelaine</li>
+                            <li> 34</li>
+                            <li> il y a 11h</li>
+                        </ul>
+                    </div>
+                    <div class="text_produit">
+                        <p> Autres détails</p>
+                    </div>
+                    
+                
+                    <div class="produit_link">
+                    <a  href=""> Réserver</a>
+                    </div>
+                
+                
+                </div>
+
+    
+            </div>
+                        </figure>
+                    </li>
+                    <li class="carousel__slide">
+                        <figure>
+                            <div>
+                                <img src="https://picsum.photos/id/1045/800/450" alt="">
+                            </div>
+                            <figcaption>
+                            <div class="card_produit_content">
+                
+                <div class="card_produit_content_texte">
+        
+                    <h3> 4 soucoupes en porcelaine</h3>
+                    <h4> 4 soucoupes en porcelaine en bon état.</h4>
+                    <hr class="produit_hr">
+                    <p> 0.50€</p>
+                    <hr class="produit_hr">
+                    <div class="card_produit_content_texte_liste">
+                        <ul>
+                            <li> Marque</li>
+                            <li> État </li>
+                            <li> Matière</li>
+                            <li> Nombres de vues</li>
+                            <li> Ajouté</li>
+                        </ul>
+                        <ul>
+                            <li> Aucune</li>
+                            <li> Bon état</li>
+                            <li> Porcelaine</li>
+                            <li> 34</li>
+                            <li> il y a 11h</li>
+                        </ul>
+                    </div>
+                    <div class="text_produit">
+                        <p> Autres détails</p>
+                    </div>
+                    
+                
+                    <div class="produit_link">
+                    <a  href=""> Réserver</a>
+                    </div>
+                
+                
+                </div>
+
+    
+            </div>
+                            </figcaption>
+                        </figure>
+                    </li>
+                    <li class="carousel__slide">
+                        <figure>
+                            <div>
+                                <img src="https://picsum.photos/id/1049/800/450" alt="">
+                            </div>
+                            <figcaption>
+                            <div class="card_produit_content">
+                
+                <div class="card_produit_content_texte">
+        
+                    <h3> 4 soucoupes en porcelaine</h3>
+                    <h4> 4 soucoupes en porcelaine en bon état.</h4>
+                    <hr class="produit_hr">
+                    <p> 0.50€</p>
+                    <hr class="produit_hr">
+                    <div class="card_produit_content_texte_liste">
+                        <ul>
+                            <li> Marque</li>
+                            <li> État </li>
+                            <li> Matière</li>
+                            <li> Nombres de vues</li>
+                            <li> Ajouté</li>
+                        </ul>
+                        <ul>
+                            <li> Aucune</li>
+                            <li> Bon état</li>
+                            <li> Porcelaine</li>
+                            <li> 34</li>
+                            <li> il y a 11h</li>
+                        </ul>
+                    </div>
+                    <div class="text_produit">
+                        <p> Autres détails</p>
+                    </div>
+                    
+                
+                    <div class="produit_link">
+                    <a  href=""> Réserver</a>
+                    </div>
+                
+                
+                </div>
+
+    
+            </div>
+                            </figcaption>
+                        </figure>
+                    </li>
+                    <li class="carousel__slide">
+                        <figure>
+                            <div>
+                                <img src="https://picsum.photos/id/1052/800/450" alt="">
+                            </div>
+                            <figcaption>
+                            <div class="card_produit_content">
+                
+                <div class="card_produit_content_texte">
+        
+                    <h3> 4 soucoupes en porcelaine</h3>
+                    <h4> 4 soucoupes en porcelaine en bon état.</h4>
+                    <hr class="produit_hr">
+                    <p> 0.50€</p>
+                    <hr class="produit_hr">
+                    <div class="card_produit_content_texte_liste">
+                        <ul>
+                            <li> Marque</li>
+                            <li> État </li>
+                            <li> Matière</li>
+                            <li> Nombres de vues</li>
+                            <li> Ajouté</li>
+                        </ul>
+                        <ul>
+                            <li> Aucune</li>
+                            <li> Bon état</li>
+                            <li> Porcelaine</li>
+                            <li> 34</li>
+                            <li> il y a 11h</li>
+                        </ul>
+                    </div>
+                    <div class="text_produit">
+                        <p> Autres détails</p>
+                    </div>
+                    
+                
+                    <div class="produit_link">
+                    <a  href=""> Réserver</a>
+                    </div>
+                
+                
+                </div>
+
+    
+            </div>
+                            </figcaption>
+                        </figure>
+                    </li>
+                </ul>    
+                <ul class="carousel__thumbnails">
+                    <li>
+                        <label for="slide-1"><img src="https://picsum.photos/id/1041/150/150" alt=""></label>
+                    </li>
+                    <li>
+                        <label for="slide-2"><img src="https://picsum.photos/id/1043/150/150" alt=""></label>
+                    </li>
+                    <li>
+                        <label for="slide-3"><img src="https://picsum.photos/id/1044/150/150" alt=""></label>
+                    </li>
+                    <li>
+                        <label for="slide-4"><img src="https://picsum.photos/id/1045/150/150" alt=""></label>
+                    </li>
+                    <li>
+                        <label for="slide-5"><img src="https://picsum.photos/id/1049/150/150" alt=""></label>
+                    </li>
+                    <li>
+                        <label for="slide-6"><img src="https://picsum.photos/id/1052/150/150" alt=""></label>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+
+
+
+        <div class="card_question">
+            <div class="card_question_title1">
+                <h2> Quelque chose à nous dire ?</h2>
+            </div>
+            <div class="card_question_title2">
+                <h3>
+                    13 commentaires
+                </h3>
+            </div>
+            <div class="card_question_form">
+                <form action="">
+                    <input type="text" placeholder="Ajouter un commentaire...">
+                </form>
+            </div>
+            <div class="card_question_reponse">
+                <p> Paul: Bonjour, quel est le prix de cette assiette ?</p>
+                <p>06/12/2023</p>
+            </div>
+            <div class="card_question_reponse">
+                <p> Jena: Bonjour, quel est le prix de cette assiette ?</p>
+                <p>06/12/2023</p>
+            </div>
+            <div class="card_question_reponse">
+                <p>Kim: Borem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per</p>
+                <p> 06/12/2023</p>
+            </div>
+        <div>
+            
+        </div>
+
+        </div>
+
+
+
+
+
+
+
+
+        
+           
+    </div>
+
+      
+    
+    </main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+
+get_footer();
+
+?>
