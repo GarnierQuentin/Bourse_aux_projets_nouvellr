ea60c0f857501e035b3af25eb30be73c4a95ad6a
Mohsen Hosen <116373904+HosenMohsen@users.noreply.github.com>
2023-12-21 16:18:21 +0100

    UPDATE mentions pc and mobile

---
+++ page-mentions-legales.php
@@ -0,0 +1,30 @@
+<?php
+get_header();
+?>
+
+<body>
+    <main>
+        <div class="card_mentions">
+            <div class="card_mentions_content">
+                <div class="card_mentions_content_title">
+                    <h2> MENTIONS LÉGALES</h2>
+                </div>
+            <div class="card_mentions_content_hr">
+                <hr>
+            </div>
+            <div class="card_mentions_content_texte">
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos vero inventore amet aliquam tempore, sunt mollitia commodi doloribus odit sed, odio nulla fugiat exercitationem vel facere dignissimos id at atque quia aspernatur aperiam neque ea officiis nemo. Id, molestias dolor illo nisi consequatur non qui dolore, voluptate rem tempore, dolorem laudantium ad! Molestias quod ad, libero eligendi odit amet natus repellendus quae nihil minus velit rem, neque, earum quas temporibus! Eligendi nobis, perspiciatis, quo facere minus architecto at accusantium unde optio eveniet dolorem alias adipisci, eum dicta culpa quis labore aspernatur nesciunt ab eaque assumenda. Eaque quis provident velit molestiae!</p>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, accusamus. Id officia dignissimos soluta sint hic eum aliquid ipsam voluptas necessitatibus quo tempora ab similique sunt architecto, a, sit impedit saepe magni eos accusantium corrupti animi facere. Amet quod similique animi tempora iure incidunt repudiandae at non, impedit a, ullam dolorem nobis dolor! Commodi, aut! Aliquam iure placeat, alias totam quo eaque libero quasi quidem exercitationem officia doloribus! Consectetur, vero? Nostrum excepturi consequatur voluptate illum perspiciatis ipsa voluptatibus vero provident.</p>
+            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum consectetur veritatis ea corrupti eos expedita incidunt quibusdam libero assumenda aut mollitia, sed placeat doloremque veniam suscipit! Delectus blanditiis iste esse asperiores fugiat optio at deserunt nobis id laudantium aliquam quis mollitia neque, debitis corporis impedit voluptatem quidem error est nostrum officiis aliquid. Veniam aliquam blanditiis illum repudiandae. Similique, molestiae quia?</p>
+            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur odio nam ipsum, cumque inventore totam quam? Officia iure illum atque, reprehenderit pariatur cum consequatur alias officiis unde itaque molestiae consectetur odit impedit qui soluta optio exercitationem adipisci repellendus laudantium in? Eaque, dolorem! Natus unde excepturi commodi quia voluptate eum esse accusamus repellendus architecto, optio rerum? Nobis quod sit similique quibusdam culpa nemo iste dignissimos, unde ad perferendis vel perspiciatis ipsum obcaecati cupiditate, dolores voluptas dolor minima porro provident commodi eos saepe? Sunt perferendis veritatis rerum pariatur obcaecati consectetur magnam exercitationem.</p>
+            </div>
+
+            </div>
+            
+        </div>
+    </main>
+</body>
+
+<?php
+get_footer();
+?>
