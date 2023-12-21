c2c0153f87b8a784babb10a819e1e4461f27086f
Quentin Garnier <113943161+GarnierQuentin@users.noreply.github.com>
2023-12-21 15:06:45 +0100

    🎨 frontend structure

--- page-nous-retrouver.php
+++ page-nous-retrouver.php
@@ -1,17 +1,79 @@
 <?php
 
 get_header();
-
-
-echo the_post_thumbnail();
 ?>
 
-<h1>VALEURS</h1>
+
+<main>
+    
+
+        <div class="valeur_title">
+            <p> Trouvez nous ici :</p>
+            <h4> 42 Av du Pdt Francois Mitterand 77200 Torcy</h4>
+        </div>
+
+        <div class="valeur_card">
+            <div class="valeur_card_content">
+                <img src="http://bap.test/wp-content/uploads/2023/12/apropos_home.png" alt="">
+                <h3> VALEURS</h3>
+                <div class="valeur_card_content_logo">
+                    <div class="valeur_card_content_logo_left">
+                        <img src="http://bap.test/wp-content/uploads/2023/12/help_green.png" alt="">
+                        <p> Lorem ipsum</p>
+                    </div>
+                    <div class="valeur_card_content_logo_right">
+                      <p> Lorem ipsum dolor sit amet consectetur, 
+                        adipisicing elit. Expedita reiciendis quae quasi 
+                        quas quo exercitationem eos harum. Nihil, dolore 
+                        expedita? Officia deserunt culpa sint. Libero, 
+                        in quos iure dolorum tempora minus aliquid facilis
+                         ab laborum officiis voluptatibus explicabo est veniam
+                          consectetur doloremque ullam possimus cumque vitae impedit esse. 
+                        Porro, culpa.</p>
+                    </div>
+                </div>
+                <div class="valeur_card_content_logo">
+                    <div class="valeur_card_content_logo_left">
+                        <img src="http://bap.test/wp-content/uploads/2023/12/tri_green.png" alt="">
+                        <p> Lorem ipsum</p>
+                    </div>
+                    <div class="valeur_card_content_logo_right">
+                      <p> Lorem ipsum dolor sit amet consectetur, 
+                        adipisicing elit. Expedita reiciendis quae quasi 
+                        quas quo exercitationem eos harum. Nihil, dolore 
+                        expedita? Officia deserunt culpa sint. Libero, 
+                        in quos iure dolorum tempora minus aliquid facilis
+                         ab laborum officiis voluptatibus explicabo est veniam
+                          consectetur doloremque ullam possimus cumque vitae impedit esse. 
+                        Porro, culpa.</p>
+                    </div>
+                </div>
+                <div class="valeur_card_content_logo">
+                    <div class="valeur_card_content_logo_left">
+                        <img src="http://bap.test/wp-content/uploads/2023/12/communication_green.png" alt="">
+                        <p> Lorem ipsum</p>
+                    </div>
+                    <div class="valeur_card_content_logo_right">
+                      <p> Lorem ipsum dolor sit amet consectetur, 
+                        adipisicing elit. Expedita reiciendis quae quasi 
+                        quas quo exercitationem eos harum. Nihil, dolore 
+                        expedita? Officia deserunt culpa sint. Libero, 
+                        in quos iure dolorum tempora minus aliquid facilis
+                         ab laborum officiis voluptatibus explicabo est veniam
+                          consectetur doloremque ullam possimus cumque vitae impedit esse. 
+                        Porro, culpa.</p>
+                    </div>
+                </div>
+                
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+        
+    </main>
+
 
 <?php
-
-echo get_the_content();
-
 get_footer();
 
 ?>
