91de7d5cb8d22ea1140680cac9aecc86413e6713
Quentin Garnier <113943161+GarnierQuentin@users.noreply.github.com>
2023-12-21 16:50:47 +0100

    🎨 politique confidentialité frontend structure

--- page-politique-confidentialite.php
+++ page-politique-confidentialite.php
@@ -1,15 +1,79 @@
 <?php
-
 get_header();
-
 ?>
 
-<h1>NOUVELL'R POLITIQUE DE CONFIDENTIALITE</h1>
+<body>
+    <main>
+        <div class="card_confidentialite">
+                <div class="card_confidentialite_content">
+                    <div class="card_confidentialite_content_title">
+                        <h2> NOUVELL'R POLITIQUE DE CONFIDENTIALITÉ</h2>
+                    </div>
+                    <div class="card_confidentialite_content_hr">
+                        <hr>
+                    </div>
+                    <div class="card_confidentialite_content_title2">
+                        <h4>MISE A JOUR DE LA POLITIQUE DE CONFIDENTIALITE : 31/12/2023</h4>
+                    </div>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis labore aliquid sit, soluta dicta odit?</p>
+                    <div>
+                        <ul>
+                            <li>
+                            1. Généralités
+                            </li>
+                            <li>
+                            2. Généralités
+                            </li>
+                            <li>
+                            3. Généralités
+                            </li>
+                            <li>
+                            4. Généralités
+                            </li>
+                            <li>
+                            5. Généralités
+                            </li>
+                        </ul>
+                    </div>
+                   
+                <div class="card_confidentialite_content_texte"> 
+                    <h3> 1. LOREM </h3>
+                    <h4>1. 1.lorem</h4>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque architecto, impedit modi quibusdam assumenda ratione.</p>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, error.</p>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iure doloremque! Sint odit labore voluptatibus suscipit odio inventore tempore consequuntur.</p>
+                </div>
+                <div class="card_confidentialite_content_texte"> 
+                    <h3> 2. LOREM </h3>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque architecto, impedit modi quibusdam assumenda ratione.</p>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, error.</p>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iure doloremque! Sint odit labore voluptatibus suscipit odio inventore tempore consequuntur.</p>
+                </div>
+                <div class="card_confidentialite_content_texte"> 
+                    <h3> 3. LOREM </h3>
+                    <h4>1. 1.lorem</h4>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque architecto, impedit modi quibusdam assumenda ratione.</p>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, error.</p>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iure doloremque! Sint odit labore voluptatibus suscipit odio inventore tempore consequuntur.</p>
+                </div>
+                <div class="card_confidentialite_content_texte"> 
+                    <h3> 4. LOREM </h3>
+                    <h4>1. 1.lorem</h4>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque architecto, impedit modi quibusdam assumenda ratione.</p>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, error.</p>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iure doloremque! Sint odit labore voluptatibus suscipit odio inventore tempore consequuntur.</p>
+                </div>
+                <div class="card_confidentialite_content_texte"> 
+                    <h3> 5. LOREM </h3>
+                    <h4>1. 1.lorem</h4>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque architecto, impedit modi quibusdam assumenda ratione.</p>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, error.</p>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iure doloremque! Sint odit labore voluptatibus suscipit odio inventore tempore consequuntur.</p>
+                </div>
+        </div>
+    </main>
+</body>
 
 <?php
-
-echo get_the_content();
-
 get_footer();
-
 ?>
